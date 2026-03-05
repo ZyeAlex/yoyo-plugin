850f497718fe6890da225460248015ce6f08f01a
ZyeAlex <76239010@qq.com>
2026-03-05 15:52:11 +0800

    BUG修复

--- apps/agent/db.js
+++ apps/agent/db.js
@@ -3,13 +3,14 @@ import { Low } from 'lowdb'
 import { JSONFile } from 'lowdb/node'
 import fs from 'fs'
 import path from 'path'
+import setting from '#setting'
 class Slangs {
     constructor() {
         // recursive: true → 若父文件夹不存在则自动创建，且文件夹已存在时不会报错
-        if (!fs.existsSync((path.join(import.meta.dirname, '../data/db/')))) {
-            fs.mkdirSync(path.join(import.meta.dirname, '../data/db/'), { recursive: true })
+        if (!fs.existsSync(path.join(setting.path, 'data/db'))) {
+            fs.mkdirSync(path.join(setting.path, 'data/db'), { recursive: true })
         }
-        const adapter = new JSONFile(path.join(import.meta.dirname, '../data/db/slangs.json'))
+        const adapter = new JSONFile(path.join(setting.path, 'data/db/slangs.json'))
         this.db = new Low(adapter, {})
         this.db.read()
     }
